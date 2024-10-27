96dd912c6f9aa488e57c635baabffb060ec296e7
Gustavo <gustavo.machuca2201@alumnos.ubiobio.cl>
2024-10-27 15:56:08 -0300

    creacion de entidades

---
+++ backend/src/entity/encargado.entity.js
@@ -0,0 +1,54 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const EncargadoSchema = new EntitySchema({
+    name: "Encargado",
+    tableName: "encargados",
+    columns: {
+        id: {
+        type: "int",
+        primary: true,
+        generated: true,
+        },
+        nombreCompleto: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        },
+        rut: {
+        type: "varchar",
+        length: 12,
+        nullable: false,
+        unique: true,
+        },
+        email: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        unique: true,
+        },
+        createdAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        nullable: false,
+        },
+        updatedAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        onUpdate: "CURRENT_TIMESTAMP",
+        nullable: false,
+        },
+    },
+    indices: [
+        {
+        name: "IDX_ENCARGADO",
+        columns: ["id"],
+        unique: true,
+        },
+        {
+        name: "IDX_ENCARGADO_RUT",
+        columns: ["rut"],
+        unique: true,
+        },
+    ],
+    });
---
+++ backend/src/entity/proveedores.entity.js
@@ -0,0 +1,64 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const ProveedorSchema = new EntitySchema({
+    name: "Proveedor",
+    tableName: "proveedores",
+    columns: {
+        id: {
+        type: "int",
+        primary: true,
+        generated: true,
+        },
+        nombre: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        },
+        email: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        unique: true,
+        },
+        telefono: {
+        type: "varchar",
+        length: 12,
+        nullable: false,
+        },
+        direccion: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        },
+        createdAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        nullable: false,
+        },
+        updatedAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        onUpdate: "CURRENT_TIMESTAMP",
+        nullable: false,
+        },
+    },
+    indices: [
+        {
+        name: "IDX_PROVEEDOR",
+        columns: ["id"],
+        unique: true,
+        },
+        {
+        name: "IDX_PROVEEDOR_EMAIL",
+        columns: ["email"],
+        unique: true,
+        },
+        {
+        name: "IDX_PROVEEDOR_TELEFONO",
+        columns: ["telefono"],
+        unique: true,
+        },
+     
+    ],
+    });
