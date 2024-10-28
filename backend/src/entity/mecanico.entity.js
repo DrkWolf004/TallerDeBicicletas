c8e0c7063a1fb59c60a22e60cfeb0c24fc79f7c6
Cristian Gavilan <cristian.gavilan1701@alumnos.ubiobio.cl>
2024-10-28 17:33:50 -0300

    ADD entities

---
+++ backend/src/entity/mecanico.entity.js
@@ -0,0 +1,83 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const MecanicoSchema = new EntitySchema({
+    name: "Mecanico",
+    tableName: "mecanicos",
+    columns: {
+        id: {
+        type: "int",
+        primary: true,
+        generated: true,
+        },
+        nombreCompleto: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        },
+        rut: {
+        type: "varchar",
+        length: 12,
+        nullable: false,
+        unique: true,
+        },
+        email: {
+        type: "varchar",
+        length: 255,
+        nullable: false,
+        unique: true,
+        },
+        disponibilidad: {
+        type: "boolean",
+        nullable: false,
+        },
+        telefono: {
+        type: "varchar",
+        length: 12,
+        nullable: false,
+        },
+        horas: {
+        type: "int",
+        nullable: false,
+        },
+        createdAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        nullable: false,
+        },
+        updatedAt: {
+        type: "timestamp with time zone",
+        default: () => "CURRENT_TIMESTAMP",
+        onUpdate: "CURRENT_TIMESTAMP",
+        nullable: false,
+        },
+    },
+    indices: [
+        {
+        name: "IDX_MECANICO",
+        columns: ["id"],
+        unique: true,
+        },
+        {
+        name: "IDX_MECANICO_RUT",
+        columns: ["rut"],
+        unique: true,
+        },
+        {
+        name: "IDX_MECANICO_EMAIL",
+        columns: ["email"],
+        unique: true,
+        },
+        {
+        name: "IDX_MECANICO_TELEFONO",
+        columns: ["telefono"],
+        unique: true,
+        },
+        {
+        name: "IDX_MECANICO_DISPONIBILIDAD",
+        columns: ["disponibilidad"],
+        },
+    ],
+});
+
+export default MecanicoSchema;
